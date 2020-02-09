77c9c5a3425e906b628839f4e875230c3b6a66ae
fainz777 <fainz777@gmail.com>
2020-02-09 14:59:25 +0300

    feat: question layout improvements

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,26 +2,27 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Skeleton from '@material-ui/lab/Skeleton';
-
-
+import Avatar from '@material-ui/core/Avatar';
+import CardHeader from '@material-ui/core/CardHeader';
 
 export class Question extends React.Component {
 	render() {
 		return (
 			<Card>
-				<CardContent>
-					{this.props.isNextStepAvailable ? (
-						<img alt={this.props.name} src={this.props.image} />
-					) : (
-						<Skeleton variant="circle" width={200} height={200} />
-					)}
-					<div>
-						{this.props.isNextStepAvailable ? this.props.name : '******'}
-					</div>
-					<div>
+				<CardHeader
+					avatar={
+						this.props.isNextStepAvailable ? (
+							<img alt={this.props.name} src={this.props.image} height={100} />
+						) : (
+							<Skeleton variant="circle" width={100} height={100} />
+						)
+					}
+
+					title={this.props.isNextStepAvailable ? this.props.name : '******'}
+					subheader={
 						<audio src={this.props.song} controls />
-					</div>
-				</CardContent>
+					}
+				/>
 			</Card>
 		);
 	}
