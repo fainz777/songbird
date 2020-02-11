54e91c9b82596efcd75c526586d4ac1d2b526db8
fainz777 <fainz777@gmail.com>
2020-02-11 23:05:42 +0300

    feat: maximum score animation

--- src/components/_layout/Finish.js
+++ src/components/_layout/Finish.js
@@ -3,21 +3,24 @@ import Grid from "@material-ui/core/Grid";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import GratsImg from '../../assets/img/grats.webp'
 
 export class Finish extends Component {
 	render() {
+		const isMaxScore = this.props.score === this.props.maxScore;
 		return (
 			<main>
 				<Grid container spacing={3} alignItems="stretch">
 					<Grid item xs={12}>
 						<Card>
-							<CardContent>
+							<CardContent style={{textAlign: 'center'}}>
 								<Typography variant="h2" gutterBottom>
 									Поздравляем!
 								</Typography>
 								<Typography variant="body1" gutterBottom>
 									Вы прошли викторину и набрали {this.props.score} из {this.props.maxScore} возможных баллов
 								</Typography>
+								{isMaxScore ? (<img src={GratsImg} />) : (null) }
 							</CardContent>
 						</Card>
 					</Grid>
